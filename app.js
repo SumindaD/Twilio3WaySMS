68323158edb693f551ab994a9e5c5fe478b79fb8
Suminda De Silva <sumindaniro@gmail.com>
2019-06-11 17:17:16 +0530

    Integrated Twilio 3 way SMS

--- app.js
+++ app.js
@@ -1,50 +1,76 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 const path = require('path');
 const Twilio = require('twilio');
+const dotenv = require('dotenv');
 
-// Access Token used for Video, IP Messaging, and Sync
-const AccessToken = Twilio.jwt.AccessToken;
-const ChatGrant = AccessToken.ChatGrant;
-const VideoGrant = AccessToken.VideoGrant;
+dotenv.config();
 
+const client = Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const MessagingResponse = Twilio.twiml.MessagingResponse;
 
 const port = 3000
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+var hostCookMap = new Map();
 
-app.get('/token', (req, res) => {
-    //const id = req.params.id;
-    console.log('Recieved Request');
-    // Create an access token which we will sign and return to the client
-  const token = new AccessToken(
-    'AC22ef054e6b6df50d0c743fbe6da35b10',
-    'SK175ae5f54c9790d8ec08ac6c0eab28c3',
-    'LOdXR8TLYAnZ8UcnGgo3hwI7v6FoWUuD'
-  );
+app.get('/CreateSMSChannel', (req, res) => {
+  hostCookMap.set(req.query.from, req.query.to);
 
-  // Assign the provided identity or generate a new one
-  token.identity = 'Sumi';
+  sendSMS('You can start texting with ' + req.query.chatwith + ' via this number... ' + process.env.TWILIO_NUMBER, process.env.TWILIO_NUMBER, req.query.from);
 
-  // Grant the access token Twilio Video capabilities
-  const videoGrant = new VideoGrant();
-  token.addGrant(videoGrant);
-
-  if (true) {
-    // Create a "grant" which enables a client to use IPM as a given user,
-    // on a given device
-    const chatGrant = new ChatGrant({
-      serviceSid: 'ISb6a5669b038e4c83952f0155221b62f8'
-    });
-    token.addGrant(chatGrant);
-  }
-
-  console.log('Got Token : ' + token.toJwt());
-  // Serialize the token to a JWT string and include it in a JSON response
   res.send({
-    identity: token.identity,
-    token: token.toJwt()
+    message: 'SMS initiation text sent to your number.'
   });
-
 });
+
+app.post('/RecievedSMS', (req, res) => {
+
+  console.log("Recieved SMS");
+  
+  client.messages.list({limit: 1}).then(messages => messages.forEach(
+    m => console.log(m.body)
+  ));
+
+  client.messages.list({limit: 1}).then(messages => messages.forEach(
+    m => {
+      io.sockets.emit("RecievedSMS", { message: m.body, from: m.from, to: m.to });
+      console.log('Redirecting SMS to: ' + hostCookMap.get(m.from) + ' From: ' + process.env.TWILIO_NUMBER);
+      sendSMS(m.body, process.env.TWILIO_NUMBER, hostCookMap.get(m.from));
+    }
+  ));
+
+  
+
+  //sendSMS('You can start texting with ' + req.query.chatwith + ' via this number... ' + process.env.TWILIO_NUMBER, process.env.TWILIO_NUMBER, req.query.from);
+
+  // const twiml = new MessagingResponse();
+
+  // twiml.message('The Robots are coming! Head for the hills!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end();
+});
+
+server.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+io.on('connection', function(client) {
+  console.log('Client connected. Id:' + client.id);
+
+  client.on('disconnect', function () {
+      console.log('Client disconnected. Id: ' + client.id);
+  });
+});
+
+function sendSMS(message, from, to){
+  client.messages
+  .create({
+      body: message,
+      from: from,
+      to: to
+    })
+  .then(message => console.log(message.sid));
+};
